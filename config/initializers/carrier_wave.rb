903e9867cd79ea686e89bc9059b240552300102e
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-06-16 00:37:06 +0300

    env7

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -7,9 +7,9 @@ CarrierWave.configure do |config|
         :provider => 'AWS',
         :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
         :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-        :region => 'ap-northeast-1'
+        :region => 'eu-central-1'
     }
-    config.fog_directory = ENV['worldofsmall-assets']
+    config.fog_directory = ENV['worldofsmall-assets/uploads']
     config.storage = :fog
   else
     config.storage = :file
