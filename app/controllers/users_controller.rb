72b4e0850fb24fc2834da2ebc11e8459fb2744c0
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-06-18 22:29:51 +0300

    signup and reviews added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.errors.any?
+      flash.now[:danger] = @user.errors.full_messages
+      render 'new'
+    else
+      log_in(@user)
+      redirect_to user_path(@user)
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :lastname, :email, :password, :password_confirmation)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,5 @@
 class Book < ApplicationRecord
+  validates :name, :author, :info, presence: true
   mount_uploader :image, ImageUploader
   has_many :reviews, dependent: :destroy
 end
