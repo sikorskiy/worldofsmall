aa739070c958683ebf4e46a08305408673259f08
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-06-27 12:25:40 +0300

    About changed and private profile added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_160626) do
+ActiveRecord::Schema.define(version: 2018_06_25_212345) do
 
   create_table "books", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2018_06_22_160626) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_private", default: false
   end
 
   create_table "wishbooks", force: :cascade do |t|
