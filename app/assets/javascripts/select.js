84f606e3e85c94f3c82855e9036156cf23bddad6
Vasiliy Sikorskiy <vasiliy.sikorskiy@gmail.com>
2018-07-20 19:36:38 +0300

    filters corrected

--- app/assets/javascripts/select.js
+++ app/assets/javascripts/select.js
@@ -2,6 +2,39 @@
 document.addEventListener("turbolinks:load", function() { //don't know why, but this is some turbolinks issues
     $('.js-select').select2({
         theme: "bootstrap",
-        allowClear: true
+        tags: true,
+        allowClear: true,
+        width: "100%",
+
+        createTag: function (params) {
+            var term = $.trim(params.term);
+            console.log(term);
+            if (term === '') {
+                return null;
+            }
+
+            return {
+                id: term,
+                text: "Новый: "+ term
+            }
+        }
+
     });
+
+    $('#filter_form').validate({
+        rules: {
+          age: {
+              min: 0,
+              max: 20
+          },
+          rating: {
+              range: [0, 5]
+          }
+        },
+        messages: {
+            age: "Некорректно указан возраст",
+            rating: "Некорретно указан рейтинг"
+        }
+    })
 });
+
